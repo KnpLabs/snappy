123ec5920f7931ca5ad3c5536157ed28258fa2c0
Matthieu Bontemps <matthieu.bontemps@gmail.com>
2010-06-27 12:53:22 +0200

    Use namespaces and add a constructor

--- lib/Snappy/Pdf.php
+++ lib/Snappy/Pdf.php
@@ -1,17 +1,21 @@
 <?php
 
+namespace Snappy;
+
 /**
  * Use this class to transform a html/a url to a pdf
  *
  * @package Snappy
  * @author Matthieu Bontemps<matthieu.bontemps@knplabs.com>
  */
-class SnappyPdf extends SnappyMedia
+class Pdf extends Media
 {
     protected $defaultExtension = 'pdf';
     protected $options = array(
         'ignore-load-errors' => null,                          // old v0.9
-        'lowquality' => true,
+        'lowquality' => null,
+        'copies' => null,
+        'dpi' => null,
     );
     
 }
