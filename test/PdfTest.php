2532b8816cc97cf4705bf2ca18650046dfa4a11e
Merlijn Tishauser <merlijn@ccadd.nl>
2011-06-21 13:50:45 +0200

    marked test as incomplete
    removed the unnecessary test for abstract parent class and placed a placeholder test marking this test class as incomplete

--- test/PdfTest.php
+++ test/PdfTest.php
@@ -1,15 +1,17 @@
 <?php
 /**
-* Test class for pdf
-*/
+ *  Test class for the PDF class.
+ *  Should only contain specific unittests for the pdf functionality
+ *  basic methods of the parentclass "Media" are/should be tested in the
+ *  Image class tests.
+ */
 
 class PdfTest extends PHPUnit_Framework_TestCase
 {
 
-	public function testParentIsAbstract()
+	public function testToBeImplemented()
 	{
-	    $rClass = new ReflectionClass('\Knplabs\Snappy\Pdf');
-    	$this->assertTrue($rClass->getParentClass()->isAbstract());
+		$this->markTestIncomplete('Unittests for the class PDF needs to be implemented');
 	}
 
 
