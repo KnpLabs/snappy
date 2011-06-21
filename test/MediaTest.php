99397acd1db9b560a66239ef6d8fe4b15241e1ba
Merlijn Tishauser <merlijn@ccadd.nl>
2011-06-21 10:39:37 +0200

    phpunit testclass added
    Added phpunit testclass for the media class. Because the media class is an abstract class direct testing is not possible. Methods from the media class witl be primarly tested by the image test class.

--- test/MediaTest.php
+++ test/MediaTest.php
@@ -1,12 +1,17 @@
 <?php
+/**
+* Only one test makes sense...are we an abstract or not.
+* The behaviour of the other classes depend on this.
+*/
+
 class MediaTest extends PHPUnit_Framework_TestCase
 {
-	public function testIsUnitTestWorkink ()
-	{
-	
-		$this->assertTrue(true);
-	}
 
+	public function testIfIsAbstract()
+	{
+	    $rClass = new ReflectionClass('\Knplabs\Snappy\Media');
+    	$this->assertTrue($rClass->isAbstract());
+	}
 
 
 }
