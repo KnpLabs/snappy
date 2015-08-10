033ccefe40ea168834d470019f9e05c59d18f935
Christian Raue <christian.raue@gmail.com>
2015-08-10 02:06:24 +0200

    fix tests on Windows

--- test/Knp/Snappy/PdfTest.php
+++ test/Knp/Snappy/PdfTest.php
@@ -4,6 +4,8 @@ namespace Knp\Snappy;
 
 class PdfTest extends \PHPUnit_Framework_TestCase
 {
+    const SHELL_ARG_QUOTE_REGEX = '(?:"|\')'; // escapeshellarg produces double quotes on Windows, single quotes otherwise
+
     public function testCreateInstance()
     {
         $testObject = new \Knp\Snappy\Pdf();
@@ -12,22 +14,24 @@ class PdfTest extends \PHPUnit_Framework_TestCase
 
     public function testThatSomething()
     {
+        $q = self::SHELL_ARG_QUOTE_REGEX;
         $testObject = new PdfSpy();
 
         $testObject->getOutputFromHtml('<html></html>', array('footer-html' => 'footer'));
-        $this->assertRegExp("/emptyBinary --lowquality --footer-html '.*' '.*' '.*'/", $testObject->getLastCommand());
+        $this->assertRegExp('/emptyBinary --lowquality --footer-html '.$q.'.*'.$q.' '.$q.'.*'.$q.' '.$q.'.*'.$q.'/', $testObject->getLastCommand());
 
         $testObject->getOutputFromHtml('<html></html>', array());
-        $this->assertRegExp("/emptyBinary --lowquality '.*' '.*'/", $testObject->getLastCommand());
+        $this->assertRegExp('/emptyBinary --lowquality '.$q.'.*'.$q.' '.$q.'.*'.$q.'/', $testObject->getLastCommand());
     }
 
     public function testThatSomethingUsingTmpFolder()
     {
+        $q = self::SHELL_ARG_QUOTE_REGEX;
         $testObject = new PdfSpy();
         $testObject->setTemporaryFolder(__DIR__);
 
         $testObject->getOutputFromHtml('<html></html>', array('footer-html' => 'footer'));
-        $this->assertRegExp("/emptyBinary --lowquality --footer-html '.*' '.*' '.*'/", $testObject->getLastCommand());
+        $this->assertRegExp('/emptyBinary --lowquality --footer-html '.$q.'.*'.$q.' '.$q.'.*'.$q.' '.$q.'.*'.$q.'/', $testObject->getLastCommand());
     }
 
     public function testThatSomethingUsingNonexistentTmpFolder()
@@ -40,12 +44,12 @@ class PdfTest extends \PHPUnit_Framework_TestCase
 
     public function testOptionsAreCorrectlySavedIfItIsLocalOrRemoteContent()
     {
+        $q = self::SHELL_ARG_QUOTE_REGEX;
         $testObject = new PdfSpy();
         $testObject->setTemporaryFolder(__DIR__);
 
         $testObject->getOutputFromHtml('<html></html>', array('footer-html' => 'footer', 'xsl-style-sheet' => 'http://google.com'));
-        $this->assertRegExp("/emptyBinary --lowquality --footer-html '.*.html' --xsl-style-sheet '.*.xsl' '.*.html' '.*.pdf'/", $testObject->getLastCommand());
-
+        $this->assertRegExp('/emptyBinary --lowquality --footer-html '.$q.'.*\.html'.$q.' --xsl-style-sheet '.$q.'.*\.xsl'.$q.' '.$q.'.*\.html'.$q.' '.$q.'.*\.pdf'.$q.'/', $testObject->getLastCommand());
     }
 
     public function testRemovesLocalFilesOnDestruct()
