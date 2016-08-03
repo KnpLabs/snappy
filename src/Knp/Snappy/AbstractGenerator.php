e0f04b8ef8b0b060c2139ed3c3fc6d8d7ff53fa9
Wouter Bulten <wouterbulten@mai-ai.org>
2016-08-03 11:17:16 +0200

    Use PHP_MAXPATHLEN instead of int
    See http://php.net/manual/en/reserved.constants.php

--- src/Knp/Snappy/AbstractGenerator.php
+++ src/Knp/Snappy/AbstractGenerator.php
@@ -591,7 +591,7 @@ abstract class AbstractGenerator implements GeneratorInterface
      */
     protected function isFile($filename)
     {
-        return strlen($filename) <= 4096 && is_file($filename);
+        return strlen($filename) <= PHP_MAXPATHLEN && is_file($filename);
     }
 
     /**
