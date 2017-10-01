b50f398bad30118477d29506ed52f5a35b76d345
Albin Kerouanton <albin.kerouanton@knplabs.com>
2017-10-01 23:34:00 +0200

    Use latest stable version of phpunit

--- test/Knp/Snappy/ImageTest.php
+++ test/Knp/Snappy/ImageTest.php
@@ -2,18 +2,13 @@
 
 namespace Knp\Snappy;
 
-class ImageTest extends \PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class ImageTest extends TestCase
 {
     public function testCreateInstance()
     {
         $testObject = new \Knp\Snappy\Image();
         $this->assertInstanceOf('\Knp\Snappy\Image', $testObject);
-    }
-
-    public function testAvailableOptions()
-    {
-        $testObject = new \Knp\Snappy\Image();
-        $testObject->setOption('use-xserver', true);
-        $testObject->setOption('enable-smart-width', true);
     }
 }
