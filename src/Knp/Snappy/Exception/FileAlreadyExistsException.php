26a392ec029b0a6bdf03f016576e31f1516b9d7c
Albin Kerouanton <albin.kerouanton@knplabs.com>
2017-10-02 01:49:53 +0200

    Add Exception interface

--- src/Knp/Snappy/Exception/FileAlreadyExistsException.php
+++ src/Knp/Snappy/Exception/FileAlreadyExistsException.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Knp\Snappy\Exception;
 
-class FileAlreadyExistsException extends \InvalidArgumentException
+use Knp\Snappy\Exception;
+
+/**
+ * Exception thrown when a file exists but was expected to not be.
+ */
+class FileAlreadyExistsException extends \InvalidArgumentException implements Exception
 {
 }
--- src/Knp/Snappy/Exception/FileNotFound.php
+++ src/Knp/Snappy/Exception/FileNotFound.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Knp\Snappy\Exception;
 
+use Knp\Snappy\Exception;
+
 /**
  * Exception thrown when a file was excepted but not found.
  *
  * @author Albin Kerouanton <albin.kerouanton@knplabs.com>
  */
-class FileNotFound extends \RuntimeException
+class FileNotFound extends \RuntimeException implements Exception
 {
     /**
      * Creates a FileNotFound instance with a standardized exception message.
