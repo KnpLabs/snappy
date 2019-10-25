7271454a76c00f6410e26089878decc95963ace5
Quentin Roques <quentin.roques@knplabs.com>
2019-10-25 13:57:49 +0200

    add resetOption method

--- src/Knp/Snappy/AbstractGenerator.php
+++ src/Knp/Snappy/AbstractGenerator.php
@@ -349,7 +349,8 @@ abstract class AbstractGenerator implements GeneratorInterface
         if (!$this->fileExists($output)) {
             throw new \RuntimeException(sprintf(
                 'The file \'%s\' was not created (command: %s).',
-                $output, $command
+                $output,
+                $command
             ));
         }
 
@@ -357,7 +358,8 @@ abstract class AbstractGenerator implements GeneratorInterface
         if (0 === $this->filesize($output)) {
             throw new \RuntimeException(sprintf(
                 'The file \'%s\' was created but is empty (command: %s).',
-                $output, $command
+                $output,
+                $command
             ));
         }
     }
@@ -380,7 +382,10 @@ abstract class AbstractGenerator implements GeneratorInterface
                 . 'stderr: "%s"' . "\n"
                 . 'stdout: "%s"' . "\n"
                 . 'command: %s.',
-                $status, $stderr, $stdout, $command
+                $status,
+                $stderr,
+                $stdout,
+                $command
             ), $status);
         }
     }
@@ -554,7 +559,8 @@ abstract class AbstractGenerator implements GeneratorInterface
             if (!$this->isFile($filename)) {
                 throw new \InvalidArgumentException(sprintf(
                     'The output file \'%s\' already exists and it is a %s.',
-                    $filename, $this->isDir($filename) ? 'directory' : 'link'
+                    $filename,
+                    $this->isDir($filename) ? 'directory' : 'link'
                 ));
             } elseif (false === $overwrite) {
                 throw new Exceptions\FileAlreadyExistsException(sprintf(
@@ -685,5 +691,16 @@ abstract class AbstractGenerator implements GeneratorInterface
     protected function mkdir($pathname)
     {
         return mkdir($pathname, 0777, true);
+    }
+
+    /**
+     * Reset all options to their initial values
+     *
+     * @return void
+     */
+    public function resetOptions()
+    {
+        $this->options = [];
+        $this->configure();
     }
 }
