d92a730a4a3557ad7a57724fcac40ecf20e5b633
Michel Roux <michel.roux@knplabs.com>
2023-06-23 16:28:26 +0200

    feat: add dompdf

--- src/Backend/WkHtmlToPdf/WkHtmlToPdf.php
+++ src/Backend/WkHtmlToPdf/WkHtmlToPdf.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace KnpLabs\Backend\WkHtmlToPdf;
 
 use ArrayAccess;
-use KnpLabs\Core\FileToPdf;
+use KnpLabs\Snappy\Core\FileToPdf;
 use Psr\Http\Message\StreamInterface;
 use SplFileInfo;
 
 final class WkHtmlToPdf implements FileToPdf
 {
-    public function generate(SplFileInfo $file, ArrayAccess|array $options = []): StreamInterface
+    public function generateFromFile(SplFileInfo $file, ArrayAccess|array $options = []): StreamInterface
     {
     }
 }
