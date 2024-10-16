71e98d4434c1d6d6809ab10a0e4302969c5ea843
alexpozzi <alexpozzi@users.noreply.github.com>
2024-10-16 17:36:52 +0200

    fix: wkhtmltopdf factory

--- src/Backend/WkHtmlToPdf/WkHtmlToPdfFactory.php
+++ src/Backend/WkHtmlToPdf/WkHtmlToPdfFactory.php
@@ -1,12 +1,14 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace KNPLabs\Snappy\Backend\WkHtmlToPdf;
 
 use KNPLabs\Snappy\Core\Backend\Adapter;
 use KNPLabs\Snappy\Core\Backend\Factory;
 use KNPLabs\Snappy\Core\Backend\Options;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriFactoryInterface;
 
 /**
  * @implements Factory<WkHtmlToPdfAdapter>
@@ -17,7 +19,13 @@ final class WkHtmlToPdfFactory implements Factory
      * @param non-empty-string $binary
      * @param positive-int     $timeout
      */
-    public function __construct(private readonly string $binary, private readonly int $timeout) {}
+    public function __construct(
+        private readonly string $binary,
+        private readonly int $timeout,
+        private readonly StreamFactoryInterface $streamFactory,
+        private readonly UriFactoryInterface $uriFactory,
+    ) {
+    }
 
     public function create(Options $options): Adapter
     {
@@ -26,6 +34,8 @@ final class WkHtmlToPdfFactory implements Factory
             $this->timeout,
             $this,
             $options,
+            $this->streamFactory,
+            $this->uriFactory,
         );
     }
 }
